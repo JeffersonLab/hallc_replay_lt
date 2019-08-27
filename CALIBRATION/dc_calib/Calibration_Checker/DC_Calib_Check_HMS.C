6365461c0b52cb5613b72920bff31177acdabf13
Stephen Kay <sjdkay@jlab.org>
2019-08-27 18:33:09 -0400

    Further updates to calibration checker script. Now runs on part 1 and 2 root files and does combined plots. Saved PDF is having issues currently

--- CALIBRATION/dc_calib/Calibration_Checker/DC_Calib_Check_HMS.C
+++ CALIBRATION/dc_calib/Calibration_Checker/DC_Calib_Check_HMS.C
@@ -19,18 +19,18 @@ void DC_Calib_Check_HMS::SlaveBegin(TTree * /*tree*/)
   printf("\n\n");
   TString option = GetOption();
 
-  h1_1u1_DriftDistance = new TH1F("1u1_DriftDistance", "Drift Distance 1u1 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_1u2_DriftDistance = new TH1F("1u2_DriftDistance", "Drift Distance 1u2 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_1x1_DriftDistance = new TH1F("1x1_DriftDistance", "Drift Distance 1x1 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_1x2_DriftDistance = new TH1F("1x2_DriftDistance", "Drift Distance 1x2 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_1v1_DriftDistance = new TH1F("1v1_DriftDistance", "Drift Distance 1v1 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_1v2_DriftDistance = new TH1F("1v2_DriftDistance", "Drift Distance 1v2 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_2u1_DriftDistance = new TH1F("2u1_DriftDistance", "Drift Distance 2u1 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_2u2_DriftDistance = new TH1F("2u2_DriftDistance", "Drift Distance 2u2 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_2x1_DriftDistance = new TH1F("2x1_DriftDistance", "Drift Distance 2x1 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_2x2_DriftDistance = new TH1F("2x2_DriftDistance", "Drift Distance 2x2 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_2v1_DriftDistance = new TH1F("2v1_DriftDistance", "Drift Distance 2v1 Plane; Drift Distance; Counts", 100, 0, 1);
-  h1_2v2_DriftDistance = new TH1F("2v2_DriftDistance", "Drift Distance 2v2 Plane; Drift Distance; Counts", 100, 0, 1);
+  h1_1u1_DriftDistance = new TH1F("1u1_DriftDistance", "Drift Distance 1u1 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_1u2_DriftDistance = new TH1F("1u2_DriftDistance", "Drift Distance 1u2 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_1x1_DriftDistance = new TH1F("1x1_DriftDistance", "Drift Distance 1x1 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_1x2_DriftDistance = new TH1F("1x2_DriftDistance", "Drift Distance 1x2 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_1v1_DriftDistance = new TH1F("1v1_DriftDistance", "Drift Distance 1v1 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_1v2_DriftDistance = new TH1F("1v2_DriftDistance", "Drift Distance 1v2 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_2u1_DriftDistance = new TH1F("2u1_DriftDistance", "Drift Distance 2u1 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_2u2_DriftDistance = new TH1F("2u2_DriftDistance", "Drift Distance 2u2 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_2x1_DriftDistance = new TH1F("2x1_DriftDistance", "Drift Distance 2x1 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_2x2_DriftDistance = new TH1F("2x2_DriftDistance", "Drift Distance 2x2 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_2v1_DriftDistance = new TH1F("2v1_DriftDistance", "Drift Distance 2v1 Plane; Drift Distance; Counts", 120, 0, 0.6);
+  h1_2v2_DriftDistance = new TH1F("2v2_DriftDistance", "Drift Distance 2v2 Plane; Drift Distance; Counts", 120, 0, 0.6);
 
   GetOutputList()->Add(h1_1u1_DriftDistance);
   GetOutputList()->Add(h1_1u2_DriftDistance);
@@ -136,7 +136,6 @@ void DC_Calib_Check_HMS::Terminate()
   TString Part_temp = option(option.Index(".")+1,option.Length());
   Int_t Run = Run_temp.Atoi(); 
   Int_t Part =  Part_temp.Atoi();
-  cout << Run << "   " << Part;
   //Need to exact the histograms from the OutputList
   TH1F* Residual[12];
   TH1F* ResidualExclPl[12];
@@ -149,64 +148,8 @@ void DC_Calib_Check_HMS::Terminate()
     Name = "h1_ResidualExclPlane_"+Plane[i];
     ResidualExclPl[i] = dynamic_cast<TH1F*> (GetOutputList()->FindObject(Name));
   }             
-  TString foutname = Form("OUTPUT/HMS_DC_Calib_Check_Run%i_Pt%i", Run, Part);
-  TString outputpdf = foutname + ".pdf";
-  TCanvas *cDC1 = new TCanvas("DC1","Drift Chamber 1 Planes",100,0,1000,900);
-  cDC1->Divide(2,3);
-  cDC1->cd(1); h1_1u1_DriftDistance->Draw();
-  cDC1->cd(2); h1_1u2_DriftDistance->Draw();
-  cDC1->cd(3); h1_1x1_DriftDistance->Draw();
-  cDC1->cd(4); h1_1x2_DriftDistance->Draw();
-  cDC1->cd(5); h1_1v1_DriftDistance->Draw();
-  cDC1->cd(6); h1_1v2_DriftDistance->Draw();
-  cDC1->Print(outputpdf + '(');               
-  TCanvas *cDC2 = new TCanvas("DC2","Drift Chamber 2 Planes",100,0,1000,900);
-  cDC2->Divide(2,3);
-  cDC2->cd(1); h1_2u1_DriftDistance->Draw();
-  cDC2->cd(2); h1_2u2_DriftDistance->Draw();
-  cDC2->cd(3); h1_2x1_DriftDistance->Draw();
-  cDC2->cd(4); h1_2x2_DriftDistance->Draw();
-  cDC2->cd(5); h1_2v1_DriftDistance->Draw();
-  cDC2->cd(6); h1_2v2_DriftDistance->Draw();
-  cDC2->Print(outputpdf);
-  TCanvas *cDC1_Res = new TCanvas("DC1_Res","Drift Chamber 1 Residuals",100,0,1000,900);
-  cDC1_Res->Divide(2,3);
-  cDC1_Res->cd(1); Residual[0]->Draw();
-  cDC1_Res->cd(2); Residual[1]->Draw();
-  cDC1_Res->cd(3); Residual[2]->Draw();
-  cDC1_Res->cd(4); Residual[3]->Draw();
-  cDC1_Res->cd(5); Residual[4]->Draw();
-  cDC1_Res->cd(6); Residual[5]->Draw();
-  cDC1_Res->Print(outputpdf);
-  TCanvas *cDC2_Res = new TCanvas("DC2_Res","Drift Chamber 2 Residuals",100,0,1000,900);
-  cDC2_Res->Divide(2,3);
-  cDC2_Res->cd(1); Residual[6]->Draw();
-  cDC2_Res->cd(2); Residual[7]->Draw();
-  cDC2_Res->cd(3); Residual[8]->Draw();
-  cDC2_Res->cd(4); Residual[9]->Draw();
-  cDC2_Res->cd(5); Residual[10]->Draw();
-  cDC2_Res->cd(6); Residual[11]->Draw();
-  cDC2_Res->Print(outputpdf);              
-  TCanvas *cDC1ResExclPl = new TCanvas("DC1ResExclPl","Drift Chamber 1 ResidualExclPls",100,0,1000,900);
-  cDC1ResExclPl->Divide(2,3);
-  cDC1ResExclPl->cd(1); ResidualExclPl[0]->Draw();
-  cDC1ResExclPl->cd(2); ResidualExclPl[1]->Draw();
-  cDC1ResExclPl->cd(3); ResidualExclPl[2]->Draw();
-  cDC1ResExclPl->cd(4); ResidualExclPl[3]->Draw();
-  cDC1ResExclPl->cd(5); ResidualExclPl[4]->Draw();
-  cDC1ResExclPl->cd(6); ResidualExclPl[5]->Draw();
-  cDC1ResExclPl->Print(outputpdf);
-  TCanvas *cDC2ResExclPl = new TCanvas("DC2ResExclPl","Drift Chamber 2 ResidualExclPls",100,0,1000,900);
-  cDC2ResExclPl->Divide(2,3);
-  cDC2ResExclPl->cd(1); ResidualExclPl[6]->Draw();
-  cDC2ResExclPl->cd(2); ResidualExclPl[7]->Draw();
-  cDC2ResExclPl->cd(3); ResidualExclPl[8]->Draw();
-  cDC2ResExclPl->cd(4); ResidualExclPl[9]->Draw();
-  cDC2ResExclPl->cd(5); ResidualExclPl[10]->Draw();
-  cDC2ResExclPl->cd(6); ResidualExclPl[11]->Draw();
-  cDC2ResExclPl->Print(outputpdf + ')');
-
-  TFile *Histogram_file = new TFile(Form("HISTOGRAMS/HMS_DCCalib_Check_Run%i.root",option.Atoi()),"RECREATE");
+  
+  TFile *Histogram_file = new TFile(Form("HISTOGRAMS/HMS_DCCalib_Check_Run%i_Pt%i.root", Run, Part),"RECREATE");
 
   h1_1u1_DriftDistance->Write();
   h1_1u2_DriftDistance->Write();

04ee5c4f5dd17c9dc248612e9f5f682a42b4267e
Stephen Kay <sjdkay@gmail.com>
2019-12-06 09:47:56 -0600

    Updated param files with second iteration of calorimeter calib script, bug in batch job script was fixed and calib re-run

--- DBASE/COIN/KaonLT_Calib/ParamFileModder2.sh
+++ DBASE/COIN/KaonLT_Calib/ParamFileModder2.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 ### Stephen Kay --- University of Regina --- 15/11/19 ###
-### Lazy boy script to move and modify the parameter files for the calorimeter ###
+### Lazy boy script to go through and modify the parameter files that will be examined ###
 ### Reads in a list of run numbers from a text file ###
 inputFile="$1"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     ##Run number#                                                         
     runNum=$line
-    cp "/home/sjdkay/work/JLab/hallc_replay_lt/CALIBRATION/hms_cal_calib/Output/Pass2/hcal.param.HMS_Cal_Calib_${runNum}_-1" "/home/sjdkay/work/JLab/hallc_replay_lt/PARAM/HMS/CAL/CALIB/hcal_${runNum}.param"
-    
+    sed -i "s/hcal_$runNum.param/hcal_8476.param/" "Offline"$runNum".param"
 done < "$inputFile"

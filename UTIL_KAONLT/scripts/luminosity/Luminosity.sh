3007440ca7bb233f9cebbed3ec215295d90643ec
Richard Trotta <trotta@cua.edu>
2020-04-03 17:07:01 -0400

    Reorganizing UTIL_KAONLT and streamlining scripts

---
+++ UTIL_KAONLT/scripts/luminosity/Luminosity.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Starting Luminosity Script"
+echo "I take as arguments the Run Number and max number of events!"
+RUNNUMBER=$1
+MAXEVENTS=$2
+if [[ $1 -eq "" ]]; then
+    echo "I need a Run Number!"
+    exit 2
+fi
+if [[ $2 -eq "" ]]; then
+    echo "Only Run Number entered...I'll assume -1 events!" 
+    MAXEVENTS=-1 
+fi
+cd ../
+#   Load params for BCM
+#   const char* CurrentFileNamePattern = "PARAM/HMS/BCM/CALIB/bcmcurrent_%d.param";
+#   gHcParms->Load(Form(CurrentFileNamePattern, RunNumber));
+# When we comment out the below bit ONLY when the bit above is commented out in replay_luminosity_coin.C
+echo -e "\n\nStarting Scaler Replay Script\n\n"
+./hcana -q "SCRIPTS/COIN/SCALERS/replay_coin_scalers.C($RUNNUMBER,-1)"
+cd CALIBRATION/bcm_current_map/
+root -b<<EOF
+.L ScalerCalib.C+
+.x run.C("../../ROOTfiles/coin_replay_scalers_${RUNNUMBER}_-1.root")
+EOF
+
+mv bcmcurrent_$RUNNUMBER.param ../../PARAM/HMS/BCM/CALIB/bcmcurrent_$RUNNUMBER.param
+cd ../../
+
+OKEVENTS=50000
+# OKEVENTS=-1
+echo -e "\n\nStarting Replay Script\n\n"
+./hcana -q "UTIL_KAONLT/scripts/luminosity/replay_lumi_coin_offline.C($RUNNUMBER,$OKEVENTS)" | tee UTIL_KAONLT/REPORT_OUTPUT/COIN/PRODUCTION/output_coin_production_${RUNNUMBER}_${OKEVENTS}.report

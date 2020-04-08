6b9a4fce1b51abb259c9cb5ccb31094a1088be6f
Richard Trotta <trotta@cua.edu>
2020-04-08 13:40:43 -0400

    Changes to lumi script using awkward arrays and changed tcoin for run periods

--- UTIL_KAONLT/scripts/luminosity/replay/replay_lumi_coin_offline.C
+++ UTIL_KAONLT/scripts/luminosity/replay/replay_lumi_coin_offline.C
@@ -37,10 +37,10 @@ void replay_lumi_coin_offline(Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   gHcParms->Load(gHcParms->GetString("g_ctp_parm_filename"));
   gHcParms->Load(gHcParms->GetString("g_ctp_kinematics_filename"), RunNumber);
   // Load params for COIN trigger configuration
-  gHcParms->Load("g_ctp_trigdet_filename");
+  // gHcParms->Load("g_ctp_trigdet_filename");
   // Load fadc debug parameters
-  gHcParms->Load("PARAM/HMS/GEN/h_fadc_debug.param");
-  gHcParms->Load("PARAM/SHMS/GEN/p_fadc_debug.param");
+  // gHcParms->Load("PARAM/HMS/GEN/h_fadc_debug.param");
+  // gHcParms->Load("PARAM/SHMS/GEN/p_fadc_debug.param");
 
   // Load params for BCM
   const char* CurrentFileNamePattern = "PARAM/HMS/BCM/CALIB/bcmcurrent_%d.param";

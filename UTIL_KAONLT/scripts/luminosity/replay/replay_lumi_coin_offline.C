91d3b0869e43e3c20cfd0d42576c2b700fd1f7c6
Richard Trotta <trotta@cua.edu>
2020-04-08 13:31:33 -0400

    Changes to lumi script using awkward arrays and changed tcoin for run periods

--- UTIL_KAONLT/scripts/luminosity/replay/replay_lumi_coin_offline.C
+++ UTIL_KAONLT/scripts/luminosity/replay/replay_lumi_coin_offline.C
@@ -170,15 +170,15 @@ void replay_lumi_coin_offline(Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   //=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=
 
   // Add Physics Module to calculate primary (scattered electrons) beam kinematics 
-  THcPrimaryKine* hkin_primary = new THcPrimaryKine("H.kin.primary", "HMS Single Arm Kinematics", "H", "H.rb");
-  THcPrimaryKine* pkin_primary = new THcPrimaryKine("P.kin.primary", "SHMS Single Arm Kinematics", "P", "P.rb");
-  gHaPhysics->Add(hkin_primary);
-  gHaPhysics->Add(pkin_primary);
+  // THcPrimaryKine* hkin_primary = new THcPrimaryKine("H.kin.primary", "HMS Single Arm Kinematics", "H", "H.rb");
+  // THcPrimaryKine* pkin_primary = new THcPrimaryKine("P.kin.primary", "SHMS Single Arm Kinematics", "P", "P.rb");
+  // gHaPhysics->Add(hkin_primary);
+  // gHaPhysics->Add(pkin_primary);
   // Add Physics Module to calculate secondary (scattered hadrons) beam kinematics
-  THcSecondaryKine* hkin_secondary = new THcSecondaryKine("H.kin.secondary", "HMS Single Arm Kinematics", "H", "P.kin.primary");
-  THcSecondaryKine* pkin_secondary = new THcSecondaryKine("P.kin.secondary", "SHMS Single Arm Kinematics", "P", "H.kin.primary");
-  gHaPhysics->Add(hkin_secondary);
-  gHaPhysics->Add(pkin_secondary);
+  // THcSecondaryKine* hkin_secondary = new THcSecondaryKine("H.kin.secondary", "HMS Single Arm Kinematics", "H", "P.kin.primary");
+  // THcSecondaryKine* pkin_secondary = new THcSecondaryKine("P.kin.secondary", "SHMS Single Arm Kinematics", "P", "H.kin.primary");
+  // gHaPhysics->Add(hkin_secondary);
+  // gHaPhysics->Add(pkin_secondary);
 
   //=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=
   // Global Objects & Event Handlers

fe4f84c87b088aeb88b1bf31a28deea9cf3ec8ce
Richard Trotta <trotta@cua.edu>
2020-04-09 23:17:50 -0400

    Fixing lumi batch scripts

--- UTIL_KAONLT/scripts/luminosity/lumiyield.py
+++ UTIL_KAONLT/scripts/luminosity/lumiyield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-04-09 23:15:19 trottar"
+# Time-stamp: "2020-04-09 23:17:43 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -765,14 +765,28 @@ def analysis(PS1, PS3, thres_curr):
           
     return track_info
 
+def mergedicts(dict1, dict2):
+    for k in set(dict1.keys()).union(dict2.keys()):
+        if k in dict1 and k in dict2:
+            if isinstance(dict1[k], dict) and isinstance(dict2[k], dict):
+                yield (k, dict(mergedicts(dict1[k], dict2[k])))
+            else:
+                # If one of the values is not a dict, you can't continue merging it.
+                # Value from second dict overrides one in first and we move on.
+                yield (k, dict2[k])
+                # Alternatively, replace this with exception raiser to alert you of value conflicts
+        elif k in dict1:
+            yield (k, dict1[k])
+        else:
+            yield (k, dict2[k])
 
 def main():
 
     # combine dictionaries
     scalers = scaler(runNum, PS1, PS3, thres_curr)
     track_info = analysis(PS1, PS3, thres_curr)
-    lumi_data = {**scalers , **track_info}
-    # lumi_data  = dict(mergedicts(scalers,track_info))
+    # lumi_data = {**scalers , **track_info}
+    lumi_data  = dict(mergedicts(scalers,track_info))
 
     table  = pd.DataFrame([lumi_data], columns=lumi_data.keys())
     

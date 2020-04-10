45d3545c83414f83552c265db6f8d8b81e4aa445
Richard Trotta <trotta@cua.edu>
2020-04-09 21:43:16 -0400

    Fixing lumi batch scripts

--- UTIL_KAONLT/scripts/luminosity/lumiyield.py
+++ UTIL_KAONLT/scripts/luminosity/lumiyield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-04-09 21:33:51 trottar"
+# Time-stamp: "2020-04-09 21:43:09 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -25,7 +25,7 @@ MaxEvent=sys.argv[2]
 # MaxEvent=50000
 
 filename = "/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/scripts/luminosity/OUTPUTS/lumi_data.csv"
-rootName = "/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/lumi_coin_offline_%i_%i.root" % (runNum,MaxEvent)
+rootName = "/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/lumi_coin_offline_%s_%s.root" % (runNum,MaxEvent)
 report = "/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/REPORT_OUTPUT/COIN/PRODUCTION/Lumi_coin_replay_production_Offline_%s_%s.report" % (RunNumber,MaxEvent)
 
 thres_curr = 2.5

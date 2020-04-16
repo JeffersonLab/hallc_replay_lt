16a767daba3f31d6845fd2fdf8a920f9a755989e
Richard Trotta <trotta@cua.edu>
2020-04-15 20:09:21 -0400

    Updated lumi script for when single event wasn't set

--- UTIL_KAONLT/scripts/luminosity/src/plot_yield.py
+++ UTIL_KAONLT/scripts/luminosity/src/plot_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-04-15 20:01:55 trottar"
+# Time-stamp: "2020-04-15 20:09:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -17,7 +17,7 @@ from csv import DictReader
 import os, subprocess
 
 USER = subprocess.call(["whoami"])
-print(USER)
+print(str(USER))
 
 # inp_f = "/home/trottar/Analysis/hallc_replay_lt/UTIL_KAONLT/scripts/luminosity/OUTPUTS/lumi_data.csv"
 # out_f = "/home/trottar/Analysis/hallc_replay_lt/UTIL_KAONLT/scripts/luminosity/OUTPUTS/yield_data.csv"

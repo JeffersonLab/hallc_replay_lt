75f028639e85cc612af2fe41e1cb004d9cd9110f
Richard Trotta <trotta@cua.edu>
2020-04-22 00:37:42 -0400

    Finalizing array 2 root addition to root2py

--- UTIL_KAONLT/scripts/luminosity/src/csv2root.py
+++ UTIL_KAONLT/scripts/luminosity/src/csv2root.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-04-22 00:36:53 trottar"
+# Time-stamp: "2020-04-22 00:37:33 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -40,7 +40,7 @@ elif csv == "yield_data":
         print("Error: %s does not appear to exist." % inp_f)
     print(yield_data.keys())
     for key,val in yield_data.items():
-        yield_data[key] = val.tolist()
+        yield_data[key] = type(val)
         print(key, "====>",yield_data[key])
     print(yield_data)
     r.py2root(yield_data,out_f)

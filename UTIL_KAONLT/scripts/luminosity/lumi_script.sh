afb0d79f320133becf18365c659097f2ca7df466
Richard Trotta <trotta@cua.edu>
2020-04-22 13:35:30 -0400

    Finalizing array 2 root addition to root2py

--- UTIL_KAONLT/scripts/luminosity/lumi_script.sh
+++ UTIL_KAONLT/scripts/luminosity/lumi_script.sh
@@ -16,10 +16,10 @@ if [[ $2 -eq "" ]]; then
     MAXEVENTS=-1 
 fi
 
-source /apps/root/6.18.04/setroot_CUE.csh
 cd /group/c-kaonlt/USERS/${USER}/hallc_replay_lt/UTIL_KAONLT/scripts/luminosity/src/
+source /apps/root/6.18.04/setroot_CUE.csh
 python3 lumiyield.py ${RUNNUMBER} ${MAXEVENTS}
 
-source /apps/root/6.18.04/setroot_CUE.csh
 cd /group/c-kaonlt/USERS/${USER}/hallc_replay_lt/UTIL_KAONLT/scripts/luminosity/src/
+source /apps/root/6.18.04/setroot_CUE.csh
 python3 csv2root.py "lumi_data"

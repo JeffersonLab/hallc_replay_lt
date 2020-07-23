9c788e45f1f78d6b58c74023b5dacab6fad3204c
Stephen Kay <sjdkay@jlab.org>
2020-07-23 18:12:06 -0400

    Further tweaks. Now moved the copying of cuts param file to within loop that checks if new files actually exist. Saves spamming unchanged files

--- CALIBRATION/ref_times/RefTCuts.sh
+++ CALIBRATION/ref_times/RefTCuts.sh
@@ -54,34 +54,37 @@ root -b <<EOF
 EOF
 sleep 1
 
-if [ "$RUNNUMBER" -le "5334" ]; then
-    cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Autumn18.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
-    cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Autumn18.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
-elif [ "$RUNNUMBER" -ge "5335" -a "$RUNNUMBER" -le "7045" ]; then 
-    cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Winter18.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
-    cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Winter18.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
-elif [ "$RUNNUMBER" -ge "7046" -a "$RUNNUMBER" -le "8375" ]; then 
-    cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Spring19.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
-    cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Spring19.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
-elif [ "$RUNNUMBER" -ge "8376" ]; then 
-    cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Summer19.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
-    cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Summer19.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
-fi
-
 if [ -f "$REPLAYPATH/CALIBRATION/ref_times/RefTimeHistos_Run${RUNNUMBER}.root" ]; then
     mv "$REPLAYPATH/CALIBRATION/ref_times/RefTimeHistos_Run${RUNNUMBER}.root" "$REPLAYPATH/CALIBRATION/ref_times/Rootfiles/RefTimeHistos_Run${RUNNUMBER}.root"
 fi
 
 if [ -f "$REPLAYPATH/CALIBRATION/ref_times/HMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param" ]; then
+    if [ "$RUNNUMBER" -le "5334" ]; then
+	cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Autumn18.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
+    elif [ "$RUNNUMBER" -ge "5335" -a "$RUNNUMBER" -le "7045" ]; then 
+	cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Winter18.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
+    elif [ "$RUNNUMBER" -ge "7046" -a "$RUNNUMBER" -le "8375" ]; then 
+	cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Spring19.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
+    elif [ "$RUNNUMBER" -ge "8376" ]; then 
+	cp "$REPLAYPATH/PARAM/HMS/HODO/hhodo_cuts_Summer19.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
+    fi
     mv "$REPLAYPATH/CALIBRATION/ref_times/HMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/HMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param"
     # Copy new offset values from file, replace old values in cuts param file
     HMSNewOffset=$(sed -n '2p' "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/HMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param")
     sed -i "s/.*hhodo_adc_tdc_offset.*/${HMSNewOffset}/" "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/hhodo_cuts_${RUNNUMBER}.param"
     rm -rf "$REPLAYPATH/CALIBRATION/ref_times/HMS/Hodo/HMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param"
-
 fi
 
 if [ -f "$REPLAYPATH/CALIBRATION/ref_times/SHMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param" ]; then
+    if [ "$RUNNUMBER" -le "5334" ]; then
+	cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Autumn18.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
+    elif [ "$RUNNUMBER" -ge "5335" -a "$RUNNUMBER" -le "7045" ]; then 
+	cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Winter18.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
+    elif [ "$RUNNUMBER" -ge "7046" -a "$RUNNUMBER" -le "8375" ]; then 
+	cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Spring19.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
+    elif [ "$RUNNUMBER" -ge "8376" ]; then 
+	cp "$REPLAYPATH/PARAM/SHMS/HODO/phodo_cuts_Summer19.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"
+    fi
     mv "$REPLAYPATH/CALIBRATION/ref_times/SHMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/SHMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param"
     SHMSNewOffset=$(sed -n '2p' "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/SHMS_Hodo_AdcTdc_Offset_${RUNNUMBER}.param")
     sed -i "s/.*phodo_adc_tdc_offset.*/${SHMSNewOffset}/" "$REPLAYPATH/CALIBRATION/ref_times/SHMS/Hodo/phodo_cuts_${RUNNUMBER}.param"

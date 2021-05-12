c7b242929edc10cd197c1684e08cb3a61f7c4a31
heinricn <heinricn@uregina.ca>
2021-05-12 15:28:21 -0400

    Added new trimmed DEF files for hodoscope calibration

--- SCRIPTS/COIN/CALIBRATION/HMSHodo_Calib_Coin_Pt2.C
+++ SCRIPTS/COIN/CALIBRATION/HMSHodo_Calib_Coin_Pt2.C
@@ -233,7 +233,7 @@ void HMSHodo_Calib_Coin_Pt2 (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   // Define output ROOT file
   analyzer->SetOutFile(ROOTFileName.Data());
   // Define DEF-file+
-  analyzer->SetOdefFile("DEF-files/CALIBRATION/HMS_Calib.def");
+  analyzer->SetOdefFile("DEF-files/CALIBRATION/HMSHodo_Calib.def");
   // Define cuts file
   analyzer->SetCutFile("DEF-files/CALIBRATION/HMS_Calib_cuts.def");  // optional
   // Start the actual analysis.

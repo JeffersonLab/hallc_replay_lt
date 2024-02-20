4ebfa78295b350f985dfd6a7516e81f0e3cab9d8
Richard Trotta <trotta@cua.edu>
2024-02-20 14:48:12 -0500

    Updated KaonLT standard.kinematics with missed hadron CT offset values and pathing changed for replay production script (from General to KaonLT).

--- SCRIPTS/COIN/PRODUCTION/FullReplay_KaonLT_Phys_Prod.C
+++ SCRIPTS/COIN/PRODUCTION/FullReplay_KaonLT_Phys_Prod.C
@@ -35,7 +35,7 @@ void FullReplay_KaonLT_Phys_Prod (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   pathList.push_back("./cache");
 
   //const char* RunFileNamePattern = "raw/coin_all_%05d.dat";
-  const char* ROOTFileNamePattern = "ROOTfiles/Analysis/General/Kaon_coin_replay_production_%d_%d.root";
+  const char* ROOTFileNamePattern = "ROOTfiles/Analysis/KaonLT/Kaon_coin_replay_production_%d_%d.root";
 
   // Load global parameters
   gHcParms->Define("gen_run_number", "Run Number", RunNumber);

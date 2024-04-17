bf83ec9223d92316aca59f05cfe3984275931e66
heinricn <heinricn@uregina.ca>
2024-04-17 15:40:12 -0600

    bug fixed

--- CALIBRATION/bcm_calibration/run_bcm_calibration.sh
+++ CALIBRATION/bcm_calibration/run_bcm_calibration.sh
@@ -5,7 +5,6 @@
 ### heinricn@jlab.org
 
 echo "Starting quick script"
-bcm_cal_4.C("/d8tb3/heinricn/ROOTfiles/BCMcalib/coin_replay_scalers_16432_-1.root","16432_BCMcalib_onOffTimes",16432,"BCM1",false)
 ROOTFILE=$1
 RANGEFILE=$2
 RUNNUMBER=$3
@@ -22,19 +21,20 @@ if [[ $2 -eq "" ]]; then
     exit 2
 fi
 
+#check if files are provided
 if [[ $3 -eq "" ]]; then
-    MAXEVENTS=-1
-else
-    MAXEVENTS=$2
+    echo "please enter run number"
+    exit 2
 fi
 
-#run through all the BCMs
-echo "root -l -q \"bcm_cal_4.C($ROOTFILE,$RANGEFILE,$RUNNUMBER,\"BCM1\",\"TRUE\")\""
 
-eval "root -l -q \"bcm_cal_4.C($ROOTFILE,$RANGEFILE,$RUNNUMBER,\"BCM1\",\"TRUE\")\"" 
-eval "root -l -q \"bcm_cal_4.C($ROOTFILE,$RANGEFILE,$RUNNUMBER,\"BCM2\",\"TRUE\")\"" 
-eval "root -l -q \"bcm_cal_4.C($ROOTFILE,$RANGEFILE,$RUNNUMBER,\"BCM4A\",\"TRUE\")\"" 
-eval "root -l -q \"bcm_cal_4.C($ROOTFILE,$RANGEFILE,$RUNNUMBER,\"BCM4B\",\"TRUE\")\"" 
-eval "root -l -q \"bcm_cal_4.C($ROOTFILE,$RANGEFILE,$RUNNUMBER,\"BCM4C\",\"TRUE\")\"" 
+#run through all the BCMs
+echo "root -l -q \"bcm_cal_4.C(\"$ROOTFILE\",\"$RANGEFILE\",$RUNNUMBER,\"BCM1\",1)\"" 
+
+root -l -q  "bcm_cal_4.C(\"$ROOTFILE\",\"$RANGEFILE\",$RUNNUMBER,\"BCM1\",1)"
+root -l -q  "bcm_cal_4.C(\"$ROOTFILE\",\"$RANGEFILE\",$RUNNUMBER,\"BCM2\",1)"
+root -l -q  "bcm_cal_4.C(\"$ROOTFILE\",\"$RANGEFILE\",$RUNNUMBER,\"BCM4A\",1)"
+root -l -q  "bcm_cal_4.C(\"$ROOTFILE\",\"$RANGEFILE\",$RUNNUMBER,\"BCM4B\",1)"
+root -l -q  "bcm_cal_4.C(\"$ROOTFILE\",\"$RANGEFILE\",$RUNNUMBER,\"BCM4C\",1)"
 
 exit 0

8a23fcd2722e8e582a800686dc180f922dfe585f
heinricn <heinricn@uregina.ca>
2025-05-08 10:56:33 -0600

    checking for insanity

--- CALIBRATION/ref_times/CoinRefTimes.C
+++ CALIBRATION/ref_times/CoinRefTimes.C
@@ -6,6 +6,7 @@ Double_t pTrig4_Roc2_Raw, pTrig4_Roc2;
 Double_t pTref2;
 Double_t Cointime_ROC1_RAW, Cointime_ROC2_RAW, Ctime_ePi_Roc1, Ctime_ePi_Roc2;
 Double_t MMpi;
+Double_t bcm;
 
 TH1D *pTRIG1_ROC1_tdcTimeRaw, *pTRIG1_ROC1_tdcTime;
 TH1D *pTRIG1_ROC2_tdcTimeRaw, *pTRIG1_ROC2_tdcTime;
@@ -25,6 +26,7 @@ void fillHistos(TTree *DataTree)
     Int_t MaxEvents = DataTree->GetEntries();
     cout << "Begining to fill histograms, " << MaxEvents << " events will be processed!\n";
     int coinCounter = 0;
+    int coinCounter2 = 0;
     for(Int_t iEntry = 0; iEntry < MaxEvents; iEntry++)
     {
         DataTree->GetEntry(iEntry);
@@ -56,9 +58,13 @@ void fillHistos(TTree *DataTree)
             MMpi_hist->Fill(MMpi);
         }
         
-        if((Ctime_ePi_Roc1 > -1.252) & (Ctime_ePi_Roc1 < 1.252))
+        if((Ctime_ePi_Roc1 > ((0)-((2.004)/2.0)-(0.25))) & (Ctime_ePi_Roc1 < ((0)+((2.004)/2.0)+(0.25))))
         {
             coinCounter++;
+            if ((bcm - 70) < 10.0)
+            {
+                coinCounter2++;
+            }
         }
         
     }
@@ -102,6 +108,8 @@ void CoinRefTimes( TString rootFileName, Int_t RunNumber)
     DataTree->SetBranchAddress("CTime.ePiCoinTime_ROC2", &Ctime_ePi_Roc2);
     
     DataTree->SetBranchAddress("P.kin.secondary.MMpi", &MMpi);
+    
+    DataTree->SetBranchAddress("H.bcm.bcm2.AvgCurrent", &bcm);
     
     pTRIG1_ROC1_tdcTimeRaw = new TH1D("T.coin.pTRIG1_ROC1_tdcTimeRaw","T.coin.pTRIG1_ROC1_tdcTimeRaw",5000, 0, 10000);
     pTRIG4_ROC1_tdcTimeRaw = new TH1D("T.coin.pTRIG4_ROC1_tdcTimeRaw","T.coin.pTRIG4_ROC1_tdcTimeRaw",5000, 0, 10000);

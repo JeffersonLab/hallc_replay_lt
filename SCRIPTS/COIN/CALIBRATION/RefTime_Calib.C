c856a9d180ca035669fab5cb3dcc84b5d151e83c
heinricn <heinricn@uregina.ca>
2025-07-30 14:07:23 -0400

    removed explicit tcoin.param call is PionLT Replay scripts

--- SCRIPTS/COIN/CALIBRATION/RefTime_Calib.C
+++ SCRIPTS/COIN/CALIBRATION/RefTime_Calib.C
@@ -44,7 +44,7 @@ void RefTime_Calib (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   gHcParms->Load(gHcParms->GetString("g_ctp_parm_filename"));
   gHcParms->Load(gHcParms->GetString("g_ctp_kinematics_filename"), RunNumber);
   // Load fadc debug parameters
-  gHcParms->Load("PARAM/TRIG/tcoin.param");
+  ////gHcParms->Load("PARAM/TRIG/tcoin.param");
   gHcParms->Load("PARAM/HMS/GEN/h_fadc_debug.param");
   gHcParms->Load("PARAM/SHMS/GEN/p_fadc_debug.param");
 
